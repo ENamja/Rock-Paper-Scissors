f1b5fb9856c605cb8f568de85b7b60e6e55d5cfd
elisha <namelisha@gmail.com>
2022-10-08 16:06:23 -0700

    Finish playRound() and edit letter casing

--- script.js
+++ script.js
@@ -2,20 +2,55 @@ moves = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     num = Math.floor(Math.random() * 3);
-    return moves[num];
+    return moves[num][0].toUpperCase() + moves[num].slice(1);
 }
 
 function getPlayerChoice() {
     while (true) {
         move = prompt('Please enter rock, paper, or scissors: ').toLowerCase();
         if (moves.includes(move)) {
-            return move;
+            return move[0].toUpperCase() + move.slice(1);
         }
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    pass;
+    let announce;
+
+    if (playerSelection == computerSelection) {
+        announce = 'It\'s a tie! ' + playerSelection + ' ties with ' + computerSelection;
+    }
+    else if (playerSelection == 'Rock') {
+        switch (computerSelection) {
+            case 'Paper':
+                announce = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            case 'Scissors':
+                announce = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+                break;
+        }
+    }
+    else if (playerSelection == 'Paper') {
+        switch (computerSelection) {
+            case 'Scissors':
+                announce = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            case 'Rock':
+                announce = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+                break;
+        }
+    }
+    else {
+        switch (computerSelection) {
+            case 'Rock':
+                announce = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            case 'Paper':
+                announce = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+                break;
+        }
+    }
+    return announce;
 }
 
 function playGame() {
