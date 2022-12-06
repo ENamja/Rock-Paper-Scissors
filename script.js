f76fb2f86b026e1bd6e68381d81760d2e9509945
elisha <namelisha@gmail.com>
2022-12-05 22:27:44 -0800

    added playGame function and display results in divs

--- script.js
+++ script.js
@@ -1,58 +1,101 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
+
+    const buttons = document.querySelectorAll("button");
+    const round = document.querySelector("div.round");
+    const player = document.querySelector("div.pScore");
+    const computer = document.querySelector("div.cScore");
+    let pScore = 0;
+    let cScore = 0;
+
     moves = ["rock", "paper", "scissors"];
 
-    function getComputerChoice() {
+    function _getComputerChoice() {
         num = Math.floor(Math.random() * 3);
         return moves[num][0].toUpperCase() + moves[num].slice(1);
     }
 
-    function playRound(playerSelection, computerSelection) {
-        let announce;
-
-        if (playerSelection == computerSelection) {
-            announce = "It\'s a tie! " + playerSelection + " ties with " + computerSelection;
+    function gameHasEnded() { // returns True if the game has ended
+        
+        if (pScore >= 5 || cScore >= 5) {
+            return true;
         }
-        else if (playerSelection == "Rock") {
-            switch (computerSelection) {
+        return false;
+    }
+
+    function playRound(e) { // Plays a singe round of rock-paper-scissors
+        let pChoice = e.target.textContent;
+        let cChoice = _getComputerChoice();
+        let display;
+
+        if (pChoice == cChoice) {
+            display = "Tie";
+        }
+        else if (pChoice == "Rock") {
+            switch (cChoice) {
                 case "Paper":
-                    announce = "You lose! " + computerSelection + " beats " + playerSelection;
+                    display = "Lose";
                     break;
                 case "Scissors":
-                    announce = "You win! " + playerSelection + " beats " + computerSelection;
+                    display = "Win";
                     break;
             }
         }
-        else if (playerSelection == "Paper") {
-            switch (computerSelection) {
+        else if (pChoice == "Paper") {
+            switch (cChoice) {
                 case "Scissors":
-                    announce = "You lose! " + computerSelection + " beats " + playerSelection;
+                    display = "Lose";
                     break;
                 case "Rock":
-                    announce = "You win! " + playerSelection + " beats " + computerSelection;
+                    display = "Win";
                     break;
             }
         }
         else {
-            switch (computerSelection) {
+            switch (cChoice) {
                 case "Rock":
-                    announce = "You lose! " + computerSelection + " beats " + playerSelection;
+                    display = "Lose";
                     break;
                 case "Paper":
-                    announce = "You win! " + playerSelection + " beats " + computerSelection;
+                    display = "Win";
                     break;
             }
         }
-        return announce;
+
+        switch (display) {
+            case "Win":
+                pScore += 1;
+                break;
+            case "Lose":
+                cScore += 1;
+                break;
+        }
+
+        round.textContent = display;
+        player.textContent = pScore;
+        computer.textContent = cScore;
     }
 
-    const buttons = document.querySelectorAll("button");
-    const div = document.querySelector("div");
+    function playGame(e) {
 
-    function logText(e) {
-        div.textContent = e.target.textContent;
+        if (!gameHasEnded()) {
+            playRound(e);
+        }
+
+        if (gameHasEnded()) { // need another if statement bc score is incremented in playRound function
+            let results = document.createElement("div");
+            results.classList.add("results");
+            document.body.appendChild(results);
+            if (pScore == 5) {
+                results.textContent = "Player Win";
+            }
+            else {
+                results.textContent = "Computer Win";
+            }
+        }
     }
 
-    buttons.forEach(button => button.addEventListener("click", logText));
+    buttons.forEach(button => button.addEventListener("click", playGame));
 
 });
